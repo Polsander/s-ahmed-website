1a3a34a9faffa8e67ef260fb17ea6261e059eb55
Polsander <oerdmann@uwaterloo.ca>
2023-01-02 22:00:58 -0800

    got backend working

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req,res)=> {
+    res.send('Hello World!')
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { console.log(`Listening on port: ${PORT}`) });
