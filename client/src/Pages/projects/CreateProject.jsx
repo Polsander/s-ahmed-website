413b3867545c8983e0fbf8511c4bc2491cebf95b
Polsander <oerdmann@uwaterloo.ca>
2023-01-06 13:10:40 -0800

    handle form submit progress

--- client/src/Pages/projects/CreateProject.jsx
+++ client/src/Pages/projects/CreateProject.jsx
@@ -14,9 +14,8 @@ const CreateProject = () => {
 
     //Handlers
     const addTextHandler = () => {
-        console.log('text');
         const component =
-            <Form.Group key={blockCounter} controlId={blockCounter}>
+            <Form.Group name='text' className='mt-2' key={blockCounter} controlId={blockCounter}>
                 <Form.Control as='textarea' />
             </Form.Group>
         const textFormComponent = {
@@ -30,9 +29,11 @@ const CreateProject = () => {
     }
 
     const addImageHandler = () => {
-        console.log('image');
         const component =
-            <p key={blockCounter}>[Image Upload Section]</p>
+            <Form.Group name='image' className='mt-3' key={blockCounter} controlId={blockCounter}>
+                <Form.Control  onInput={()=>{console.log(blockCounter)}} type='file'/>
+            </Form.Group>
+
 
         const imageFormComponent = {
             id: blockCounter,
@@ -45,8 +46,10 @@ const CreateProject = () => {
         setBlockCounter(blockCounter + 1);
     }
 
-    const postHandler = () => {
-        console.log('post submit');
+    const postHandler = (e) => {
+        console.log('Did we make it??')
+        e.preventDefault();
+        console.log(e.target[2].id);
     }
 
     return (
@@ -54,16 +57,17 @@ const CreateProject = () => {
             <Container>
                 <Button className='m-3' onClick={addTextHandler}>Add Text</Button>
                 <Button className='m-3' onClick={addImageHandler}>Add Image</Button>
-                <Form>
-                    <Form.Group controlId='title'>
+                <Form onSubmit={postHandler}>
+                    <Form.Group name='title' controlId='title'>
                         <Form.Label>Project Title</Form.Label>
                         <Form.Control type='text' />
                     </Form.Group>
-                    <Form.Group className='mt-5' controlId='main-image'>
-                        <p>[Image upload section]</p>
+                    <Form.Group name='mainImage' className='mt-5' controlId='main-image'>
+                        <Form.Label>Header Image (your main thumbnail)</Form.Label>
+                        <Form.Control type='file' />
                     </Form.Group>
                     {!formBody[0] ? null : formBody.map((element) => { return element.component })}
-                    <Button className='mt-4' type="submit" onClick={postHandler}>Post</Button>
+                    <Button className='mt-4' type="submit" >Post</Button>
                 </Form>
             </Container >
         </div>
