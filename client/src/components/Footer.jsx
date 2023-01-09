b3d2888bb12548950391f86bd22d21d6a0812e65
Polsander <oerdmann@uwaterloo.ca>
2023-01-08 23:19:54 -0800

    implemented footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import styles from './Footer.module.css';
+
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className={styles.footer}>
+            <h6 className='pt-4 ms-5'>
+                Samar Ahmed 2023 ©
+            </h6>
+        </div>
+    )
 }
 
 export default Footer
